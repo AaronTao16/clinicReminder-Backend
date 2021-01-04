3847a18bfbe8d699893a2510e1783023008a0fe3
AaronTao16 <aarontao16@outlook.com>
2021-01-04 11:08:44 -0500

    Initial Commit

--- src/main/java/com/pitt/backend/controller/GreetingController.java
+++ src/main/java/com/pitt/backend/controller/GreetingController.java
@@ -4,8 +4,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
-
 
 @Controller
 public class GreetingController {
